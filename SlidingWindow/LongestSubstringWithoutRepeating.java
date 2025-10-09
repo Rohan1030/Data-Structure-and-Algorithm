c37335d31b75033ab022dff07614a3295041f1b4
Rohan1030 <rkrk02233@gmail.com>
2025-10-09 20:19:08 +0530

    done

---
+++ SlidingWindow/LongestSubstringWithoutRepeating.java
@@ -0,0 +1,40 @@
+package SlidingWindow;
+
+import java.util.HashMap;
+
+public class LongestSubstringWithoutRepeating {
+  class Solution {
+
+    public int lengthOfLongestSubstring(String s) {
+
+        HashMap<Character,Integer> map = new HashMap<>();
+
+        int n = s.length();
+        int left=0;
+        int maxLen = 0;
+        
+        for(int right=0;right<n;right++)
+        {
+            char c = s.charAt(right);
+            if(map.containsKey(c))
+            {
+                left = Math.max(left,map.get(c)+1);
+
+            }
+             
+        
+                map.put(c,right);
+                maxLen = Math.max(maxLen , right-left+1);
+            
+            
+
+        }
+        return maxLen;
+
+
+
+        
+    }
+}
+  
+}
