00e186c85a62938d211cfe3041f48a1e5c84899f
Rohan1030 <rkrk02233@gmail.com>
2025-10-15 00:13:40 +0530

    done

---
+++ BinarySearch/SearchInsertPosition.java
@@ -0,0 +1,38 @@
+package BinarySearch;
+
+public class SearchInsertPosition {
+
+
+
+                //Leetcode :- 35
+
+
+
+  class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int n =nums.length;
+        int low =0;
+        int high = n-1;
+
+        while(low<=high)
+        {
+         int    mid = (low+high)/2;
+            if(nums[mid]==target)
+            {
+                return mid;
+            }
+            else if(target>nums[mid])
+            {
+                low = mid+1;
+            }
+            else 
+            {
+                high=mid-1;
+            }
+        }
+        return low;
+        
+    }
+}
+  
+}
