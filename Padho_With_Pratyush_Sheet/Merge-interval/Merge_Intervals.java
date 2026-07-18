794d76f9e5b2262b309fd780ce5a2b5ff1335a85
Rohan1030 <rkrk02233@gmail.com>
2026-07-18 12:50:18 +0530

    done

---
+++ Padho_With_Pratyush_Sheet/Merge-interval/Merge_Intervals.java
@@ -0,0 +1,38 @@
+    // Leetcode:-56. Merge Intervals
+
+class Solution {
+    public int[][] merge(int[][] intervals) {
+
+        // Sort intervals by start time
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+
+        List<int[]> res = new ArrayList<>();
+
+        // Initialize first interval
+        int start1 = intervals[0][0];
+        int end1 = intervals[0][1];
+
+        for (int i = 1; i < intervals.length; i++) {
+
+            int start2 = intervals[i][0];
+            int end2 = intervals[i][1];
+
+            // If intervals overlap, extend the end
+            if (end1 >= start2) {
+                end1 = Math.max(end1, end2);
+            } 
+            // No overlap, save current interval
+            else {
+                res.add(new int[]{start1, end1});
+                start1 = start2;
+                end1 = end2;
+            }
+        }
+
+        // Add the last merged interval
+        res.add(new int[]{start1, end1});
+
+        // Convert List to 2D array
+        return res.toArray(new int[res.size()][]);
+    }
+}
