a14a233b84eec8f4bf4d5412966685b4b87cddfa
Rohan1030 <rkrk02233@gmail.com>
2026-07-23 12:28:32 +0530

    Done

--- Padho_With_Pratyush_Sheet/Recursion/Palindrome_String.java
+++ Padho_With_Pratyush_Sheet/Recursion/Palindrome_String.java
@@ -1,5 +1,42 @@
 //  GFG :-  Palindrome String
 
+// Using recursion 
+class Solution {
+
+    boolean isPalindrome(String s) {
+        return check(s, 0, s.length() - 1);
+    }
+
+    // Recursive function
+    boolean check(String s, int left, int right) {
+
+        // Base case: pointers crossed or met
+        if (left >= right) {
+            return true;
+        }
+
+        // Characters don't match
+        if (s.charAt(left) != s.charAt(right)) {
+            return false;
+        }
+
+        // Check the remaining string
+        return check(s, left + 1, right - 1);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+      // Normal Approach
+
+
 class Solution {
 
     boolean isPalindrome(String s) {
